02e34fb4b94433ebb78362f878a4eeabad134035
axlfrost <ale.gonzalezlevio@gmail.com>
2019-11-23 20:08:38 -0300

    Implementación de borrado de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -17,8 +17,8 @@ app.get('/', (req, res, next) => {
 
     Usuario.find({}, 'nombre email img role').exec(
         (error, usuarios) => {
+            // Errores
             if (error) {
-
                 return res.status(500).json({
                     ok: false,
                     mensaje: 'Error cargando usuarios',
@@ -54,6 +54,7 @@ app.post('/', (req, res) => {
     });
 
     usuario.save((error, usuarioGuardado) => {
+        // Errores
         if (error) {
             return res.status(400).json({
                 ok: false,
@@ -78,46 +79,82 @@ app.put('/:id', (req, res) => {
 
     Usuario.findById(id, 'nombre email img role').exec(
         (error, usuario) => {
-        if (error) {
-            return res.status(500).json({
-                ok: false,
-                mensaje: 'Error al buscar usuario',
-                errors: error
-            });
-        }
-        if (!usuario) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: `El usuario con el ID ${id} no existe`,
-                errors: { message: 'No existe un usuario con esa ID' }
-            });
-        }
-
-
-        let body = req.body;
-
-        usuario.nombre = body.nombre;
-        usuario.email = body.email;
-        usuario.role =body.role;
-
-        usuario.save( (error, usuarioGuardado) => {
+            // Errores
             if (error) {
-                return res.status(400).json({
+                return res.status(500).json({
                     ok: false,
-                    mensaje: 'Error al actualizar usuario',
+                    mensaje: 'Error al buscar usuario',
                     errors: error
                 });
             }
-            res.status(200).json({
-                ok: true,
-                usuario: usuarioGuardado
+            // Si el usuario no existe
+            if (!usuario) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: `El usuario con el ID ${id} no existe`,
+                    errors: { message: 'No existe un usuario con esa ID' }
+                });
+            }
+
+
+            let body = req.body;
+
+            usuario.nombre = body.nombre;
+            usuario.email = body.email;
+            usuario.role = body.role;
+
+            usuario.save((error, usuarioGuardado) => {
+                if (error) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Error al actualizar usuario',
+                        errors: error
+                    });
+                }
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioGuardado
+                });
+
             });
 
         });
 
-    });
-
 })
+
+// ==========================================
+// Borrar usuario
+// ==========================================
+app.delete('/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, 
+        (error, usuarioBorrado) => {
+            // Errores
+            if (error) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar usuario',
+                    errors: error
+                });
+            }
+            // Si el usuario no existe
+            if (!usuarioBorrado) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: `No existe el usuario con el ID ${id}`,
+                    errors: { message: 'No existe un usuario con esa ID' }
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioBorrado
+            });
+        })
+});
+
 
 
 
